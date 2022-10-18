8d83de5791b1319a10ee0826f3142a356ffd2fa5
chenzhenmin <1290433700@qq.com>
2022-10-18 09:55:11 +0800

    feat: 断言新增数字符号单测

--- src/__test__/expect.test.ts
+++ src/__test__/expect.test.ts
@@ -32,4 +32,15 @@ describe("examples for jest expect", () => {
     // 小彩蛋
     expect(1 + 1).toEqual(2);
   });
+
+  test("函数", () => {});
+
+  test("数字符号", () => {
+    expect(3).toBeGreaterThan(2);
+    expect(3).toBeLessThan(4);
+    expect(3).toBeGreaterThanOrEqual(3);
+    expect(3).toBeGreaterThanOrEqual(2);
+    expect(3).toBeLessThanOrEqual(3);
+    expect(3).toBeLessThanOrEqual(4);
+  });
 });
