779cb931ed1169844708a899b418b44adc8ba7fa
陈祯民 <chenzhenmin@bytedance.com>
2022-11-05 13:52:01 +0800

    feat: 补充第六节单测

--- src/__test__/dom_expect.test.tsx
+++ src/__test__/dom_expect.test.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import { DomExpect } from "../components/DomExpect/index";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { DomExpect } from '../components/DomExpect/index';
+import { act } from 'react-dom/test-utils';
 
-describe("tests for 《6 | DOM断言:页面元素的断言》", () => {
-  test("visible validation without semi", () => {
+describe('tests for 《6 | DOM断言:页面元素的断言》', () => {
+  test('visible validation without semi', () => {
     render(<DomExpect />);
-    const emptyNote = screen.getByRole("generic", { name: "empty_note" });
-    const [hiddenNote] = screen.getAllByRole("note", { hidden: true });
-    const normalNote = screen.getByRole("note");
+    const emptyNote = screen.getByRole('generic', { name: 'empty_note' });
+    const [hiddenNote] = screen.getAllByRole('note', { hidden: true });
+    const normalNote = screen.getByRole('note');
     expect(emptyNote).toBeEmptyDOMElement();
     expect(hiddenNote).not.toBeVisible();
     expect(emptyNote).toBeInTheDocument();
@@ -16,13 +17,13 @@ describe("tests for 《6 | DOM断言:页面元素的断言》", () => {
     expect(normalNote).toHaveTextContent(/1/i);
   });
 
-  test("form validation without semi", () => {
+  test('form validation without semi', () => {
     render(<DomExpect />);
-    const form = screen.getByRole("form");
-    const username = screen.getByRole("textbox");
-    const age = screen.getByRole("spinbutton");
-    const manCheckbox = screen.getByRole("radio", { checked: true });
-    const womanCheckbox = screen.getByRole("radio", { checked: false });
+    const form = screen.getByRole('form', { name: 'form' });
+    const username = screen.getByRole('textbox', { name: 'form_username' });
+    const age = screen.getByRole('spinbutton', { name: 'form_age' });
+    const manCheckbox = screen.getByRole('radio', { checked: true, name: 'form_sex' });
+    const womanCheckbox = screen.getByRole('radio', { checked: false, name: 'form_sex' });
     expect(username).toBeDisabled();
     expect(age).toBeEnabled();
     expect(age).toBeRequired();
@@ -31,18 +32,52 @@ describe("tests for 《6 | DOM断言:页面元素的断言》", () => {
     expect(manCheckbox).toBeChecked();
     expect(womanCheckbox).not.toBeChecked();
     expect(form).toHaveFormValues({
-      username: "zhenmin",
+      username: 'zhenmin',
       age: 23,
-      sex: "man",
+      sex: 'man',
     });
     expect(age).toHaveValue(23);
   });
 
-  test("code validation", () => {
+  test('code validation', () => {
     render(<DomExpect />);
-    const [hiddenNote] = screen.getAllByRole("note", { hidden: true });
-    expect(hiddenNote).toHaveAttribute("aria-hidden");
-    expect(hiddenNote).toHaveClass("hidden");
-    expect(hiddenNote).toHaveStyle("display: none");
+    const [hiddenNote] = screen.getAllByRole('note', { hidden: true });
+    expect(hiddenNote).toHaveAttribute('aria-hidden');
+    expect(hiddenNote).toHaveClass('hidden');
+    expect(hiddenNote).toHaveStyle('display: none');
+  });
+
+  test('visible validation with semi', () => {
+    render(<DomExpect />);
+    const form = screen.getByRole('form', { name: 'semi-form' });
+    const username = screen.getByLabelText('username');
+    const age = screen.getByLabelText('age');
+    const sex = screen.getByLabelText('sex');
+    const hobby = screen.getByLabelText('hobby');
+    expect(username).toBeDisabled();
+    expect(age).toBeEnabled();
+    expect(age).toBeRequired();
+    act(() => {
+      age.focus();
+    });
+    expect(age).toHaveFocus();
+    // expect(username).toHaveValue('zhenmin');
+    // expect(hobby).toHaveValue('code');
+    // expect(form).toHaveFormValues({
+    //   username: 'zhenmin',
+    //   age: 23,
+    //   sex: 'man',
+    //   hobby: 'code',
+    // });
+    const hiddenNotes = screen.getAllByRole('note', { hidden: true });
+    expect(hiddenNotes[2]).toHaveAttribute(
+      'value',
+      JSON.stringify({
+        username: 'zhenmin',
+        age: 23,
+        sex: 'man',
+        hobby: 'code',
+      })
+    );
   });
 });
--- src/components/DomExpect/index.tsx
+++ src/components/DomExpect/index.tsx
@@ -1,41 +1,47 @@
-import { FC } from "react";
+import React, { FC, useState } from 'react';
+import { Form } from '@douyinfe/semi-ui';
 
 interface IProps {}
 
 // 《6 | DOM断言：页面元素的断言》
 export const DomExpect: FC<IProps> = ({}) => {
+  const [semiFormValues, setSemiFormValues] = useState({ username: 'zhenmin', age: 23, sex: 'man', hobby: 'code' });
+
   return (
     <div>
       {/* 页面可见 */}
-      <div aria-label="empty_note"></div>
-      <div
-        role="note"
-        style={{ display: "none" }}
-        className="test hidden"
-        aria-hidden
-      >
+      <div aria-label="empty_note" />
+      <div role="note" style={{ display: 'none' }} className="test hidden" aria-hidden>
         1234
       </div>
       <div role="note">1234</div>
       {/* 表单验证 */}
       <form aria-label="form">
-        <input
-          type="text"
-          name="username"
-          disabled
-          aria-disabled
-          defaultValue="zhenmin"
-        />
-        <input type="number" name="age" defaultValue={23} required />
-        <input
-          type="radio"
-          name="sex"
-          value="man"
-          defaultChecked
-          aria-checked
-        />
-        <input type="radio" name="sex" value="woman" />
+        <input type="text" name="username" disabled aria-disabled defaultValue="zhenmin" aria-label="form_username" />
+        <input type="number" name="age" defaultValue={23} required aria-label="form_age" />
+        <input type="radio" name="sex" value="man" defaultChecked aria-checked aria-label="form_sex" />
+        <input type="radio" name="sex" value="woman" aria-label="form_sex" />
       </form>
+      {/* semi 表单验证 */}
+      <Form
+        initValues={semiFormValues}
+        aria-label="semi-form"
+        onChange={(data: any): void => {
+          setSemiFormValues(data);
+        }}
+      >
+        <Form.Input field="username" disabled name="username" />
+        <Form.InputNumber field="age" required name="age" />
+        <Form.RadioGroup field="sex" name="sex">
+          <Form.Radio value="man" />
+          <Form.Radio value="woman" />
+        </Form.RadioGroup>
+        <Form.Select field="hobby" name="hobby">
+          <Form.Select.Option value="code">code</Form.Select.Option>
+          <Form.Select.Option value="read">read</Form.Select.Option>
+        </Form.Select>
+      </Form>
+      <input type="hidden" role="note" value={JSON.stringify(semiFormValues)} />
     </div>
   );
 };
