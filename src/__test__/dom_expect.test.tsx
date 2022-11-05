048bc349ac899ad131981c1807f5b2cbf8d828c2
chenzhenmin <1290433700@qq.com>
2022-11-05 08:31:19 +0800

    feat: 更新第6节用例

--- src/__test__/dom_expect.test.tsx
+++ src/__test__/dom_expect.test.tsx
@@ -5,15 +5,44 @@ import { DomExpect } from "../components/DomExpect/index";
 describe("tests for 《6 | DOM断言:页面元素的断言》", () => {
   test("visible validation without semi", () => {
     render(<DomExpect />);
-    // const emptyNote = screen.getByRole("generic", { name: "empty_note" });
-    // const hiddenNote = screen.getAllByRole("note", { hidden: true });
-    // const normalNote = screen.getAllByRole("note", { hidden: false });
-    // expect(hiddenNote).toHaveLength(1);
-    // expect(normalNote).toHaveLength(1);
-    // expect(emptyNote).toBeEmptyDOMElement();
-    // expect(hiddenNote[0]).not.toBeVisible();
-    // expect(emptyNote).toBeInTheDocument();
-    // expect(hiddenNote[0]).toBeInTheDocument();
-    // expect(normalNote[0]).toBeInTheDocument();
+    const emptyNote = screen.getByRole("generic", { name: "empty_note" });
+    const [hiddenNote] = screen.getAllByRole("note", { hidden: true });
+    const normalNote = screen.getByRole("note");
+    expect(emptyNote).toBeEmptyDOMElement();
+    expect(hiddenNote).not.toBeVisible();
+    expect(emptyNote).toBeInTheDocument();
+    expect(hiddenNote).toBeInTheDocument();
+    expect(normalNote).toBeInTheDocument();
+    expect(normalNote).toHaveTextContent(/1/i);
+  });
+
+  test("form validation without semi", () => {
+    render(<DomExpect />);
+    const form = screen.getByRole("form");
+    const username = screen.getByRole("textbox");
+    const age = screen.getByRole("spinbutton");
+    const manCheckbox = screen.getByRole("radio", { checked: true });
+    const womanCheckbox = screen.getByRole("radio", { checked: false });
+    expect(username).toBeDisabled();
+    expect(age).toBeEnabled();
+    expect(age).toBeRequired();
+    age.focus();
+    expect(age).toHaveFocus();
+    expect(manCheckbox).toBeChecked();
+    expect(womanCheckbox).not.toBeChecked();
+    expect(form).toHaveFormValues({
+      username: "zhenmin",
+      age: 23,
+      sex: "man",
+    });
+    expect(age).toHaveValue(23);
+  });
+
+  test("code validation", () => {
+    render(<DomExpect />);
+    const [hiddenNote] = screen.getAllByRole("note", { hidden: true });
+    expect(hiddenNote).toHaveAttribute("aria-hidden");
+    expect(hiddenNote).toHaveClass("hidden");
+    expect(hiddenNote).toHaveStyle("display: none");
   });
 });
--- src/components/DomExpect/index.tsx
+++ src/components/DomExpect/index.tsx
@@ -6,13 +6,36 @@ interface IProps {}
 export const DomExpect: FC<IProps> = ({}) => {
   return (
     <div>
+      {/* 页面可见 */}
       <div aria-label="empty_note"></div>
-      <div role="note" style={{ display: "none" }} aria-hidden>
+      <div
+        role="note"
+        style={{ display: "none" }}
+        className="test hidden"
+        aria-hidden
+      >
         1234
       </div>
-      <div role="note" aria-hidden={false}>
-        1234
-      </div>
+      <div role="note">1234</div>
+      {/* 表单验证 */}
+      <form aria-label="form">
+        <input
+          type="text"
+          name="username"
+          disabled
+          aria-disabled
+          defaultValue="zhenmin"
+        />
+        <input type="number" name="age" defaultValue={23} required />
+        <input
+          type="radio"
+          name="sex"
+          value="man"
+          defaultChecked
+          aria-checked
+        />
+        <input type="radio" name="sex" value="woman" />
+      </form>
     </div>
   );
 };
