400731606f36a95b99f4ea88bff55a6818c28a1d
chenzhenmin <1290433700@qq.com>
2022-11-28 10:40:08 +0800

    feat: 新增coverage

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,26 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  babel: (options) => {
+    return {
+      ...options,
+      plugins: [
+        [
+          "babel-plugin-istanbul",
+          {
+            include: "src/components/**/*.{js,jsx,tsx,(!d).ts}",
+          },
+        ],
+      ],
+    };
+  },
+};
--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:6006/",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("@cypress/code-coverage/task")(on, config);
+      return config;
     },
     defaultCommandTimeout: 10000,
   },
